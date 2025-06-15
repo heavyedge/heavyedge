835a01b124cbbf461e208adff29c7e5b6dbcfc43
JSS95 <52185322+JSS95@users.noreply.github.com>
2025-06-15 17:04:48 +0900

    Fix wasserstein (#13)
    * fix: wasserstein functions
    
    Add random variable to quantile()
    
    * fix: wasserstein functions
    
    Modify wdist()
    
    * fix: wasserstein functions
    
    Modify wdist()
    
    * fix: wasserstein functions
    
    Modify
    
    * fix: wasserstein functions
    
    Modify wasserstein.py
    
    * fix: wasserstein functions
    
    Modify mean()
    
    * fix: wasserstein functions
    
    Modify command
    
    * fix: wasserstein functions
    
    changelog
    
    * fix: wasserstein functions
    
    Fix doc

--- src/heavyedge/wasserstein.py
+++ src/heavyedge/wasserstein.py
@@ -6,80 +6,136 @@ Wasserstein-related functions.
 """
 
 import numpy as np
+from scipy.integrate import cumulative_trapezoid
+from scipy.interpolate import interp1d
 
 from ._wasserstein import optimize_q
 
 __all__ = [
+    "quantile",
     "wdist",
     "wmean",
 ]
 
 
-def quantile(pdf, x):
-    """Convert probability distribution function to quantile function.
+def quantile(x, f, t):
+    """Convert a probability distribution to a quantile function.
 
     Parameters
     ----------
-    pdf : ndarray
-        An 1D array of probability distribution function values.
     x : ndarray
-        An 1D array of X values for quantile function. Must be strictly increasing
-        from 0 to 1.
+        Points over which *f* is measured.
+    f : ndarray
+        The empirical probability density function.
+    t : ndarray
+        Points over which the quantile function will be measured.
+        Must be strictly increasing from 0 to 1.
 
     Returns
     -------
     ndarray
-        Quantile function of *pdf* over *x*.
+        Quantile function of *f* over *t*.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from heavyedge import get_sample_path, ProfileData
+    >>> from heavyedge.wasserstein import quantile
+    >>> with ProfileData(get_sample_path("Prep-Type2.h5")) as data:
+    ...     Y = next(data.profiles())
+    ...     x = data.x()[:len(Y)]
+    >>> f = Y / np.trapezoid(Y, x)
+    >>> t = np.linspace(0, 1, 100)
+    >>> Q = quantile(x[:len(f)], f, t)
     """
-    cdf = np.insert(np.cumsum(pdf), 0, 0)
-    return np.interp(x, cdf, np.arange(len(cdf)))
+    G = cumulative_trapezoid(f, x, initial=0)
+    return interp1d(G, x, bounds_error=False, fill_value=(x[0], x[-1]))(t)
 
 
-def wdist(G1, G2, grid_num):
+def wdist(x1, f1, x2, f2, grid_num):
     r"""Wasserstein distance between two 1D probability distributions.
 
     .. math::
 
-        d_W(G_1, G_2)^2 = \int^1_0 (G_1^{-1}(t) - G_2^{-1}(t))^2 dt
+        d_W(f_1, f_2)^2 = \int^1_0 (Q_1(t) - Q_2(t))^2 dt
+
+    where :math:`Q_i` is the quantile function of :math:`f_i`.
 
     Parameters
     ----------
-    G1, G2 : ndarray
-        The probability distribution functions of the input data.
+    x1, f1 : ndarray
+        The first empirical probability density function.
+    x2, f2 : ndarray
+        The second empirical probability density function.
     grid_num : int
-        Number of sample points in [0, 1] to compute integration.
+        Number of sample points in [0, 1] to approximate the integral.
 
     Returns
     -------
     scalar
         Wasserstein distance.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from heavyedge import get_sample_path, ProfileData
+    >>> from heavyedge.wasserstein import wdist
+    >>> with ProfileData(get_sample_path("Prep-Type2.h5")) as data:
+    ...     x = data.x()
+    ...     (Y1, Y2), (L1, L2), _ = data[:2]
+    >>> x1, Y1 = x[:L1], Y1[:L1]
+    >>> x2, Y2 = x[:L2], Y2[:L2]
+    >>> f1, f2 = Y1 / np.trapezoid(Y1, x1), Y2 / np.trapezoid(Y2, x2)
+    >>> d = wdist(x1, f1, x2, f2, 100)
     """
     grid = np.linspace(0, 1, grid_num)
-    return np.trapezoid((quantile(G1, grid) - quantile(G2, grid)) ** 2, grid) ** 0.5
+    Q1 = quantile(x1, f1, grid)
+    Q2 = quantile(x2, f2, grid)
+    return np.trapezoid((Q1 - Q2) ** 2, grid) ** 0.5
 
 
-def wmean(Y, grid_num):
-    """Fréchet mean of probability distrubution functions using Wasserstein metric.
+def wmean(xs, fs, grid_num):
+    """Fréchet mean of probability distrubutions using Wasserstein metric.
 
     Parameters
     ----------
-    Y : list of array
-        Probability distribution functions.
+    xs : list of ndarray
+        Points over which each distribution in *fs* is measured.
+    fs : list of ndarray
+        Empirical probability density functions.
     grid_num : int
         Number of sample points in [0, 1] to construct regression results.
 
     Returns
     -------
-    ndarray
-        Averaged *Y*.
+    x, f : ndarray
+        Probability density function.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from heavyedge import get_sample_path, ProfileData
+    >>> from heavyedge.wasserstein import wmean
+    >>> with ProfileData(get_sample_path("Prep-Type2.h5")) as data:
+    ...     x = data.x()
+    ...     (Y1, Y2), (L1, L2), _ = data[:2]
+    >>> x1, Y1 = x[:L1], Y1[:L1]
+    >>> x2, Y2 = x[:L2] + 3, Y2[:L2]
+    >>> f1, f2 = Y1 / np.trapezoid(Y1, x1), Y2 / np.trapezoid(Y2, x2)
+    >>> x, f = wmean([x1, x2], [f1, f2], 100)
+    >>> import matplotlib.pyplot as plt  # doctest: +SKIP
+    ... plt.plot(x1, f1, "--", color="gray")
+    ... plt.plot(x2, f2, "--", color="gray")
+    ... plt.plot(x, f)
     """
     grid = np.linspace(0, 1, grid_num)
-    Q = np.array([quantile(y, grid) for y in Y])
+    Q = np.array([quantile(x, f, grid) for x, f in zip(xs, fs)])
     g = np.mean(Q, axis=0)
-
     if np.all(np.diff(g) >= 0):
         q = g
     else:
         q = optimize_q(g)
-    cdf = np.interp(np.arange(int(q[-1])), q, np.linspace(0, 1, len(q)))
-    return np.concatenate([np.diff(cdf), [0]])
+    pdf = 1 / np.gradient(q, grid)
+    pdf[-1] = 0
+    pdf /= np.trapezoid(pdf, q)
+    return q, pdf
