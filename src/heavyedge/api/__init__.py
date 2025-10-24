8ff59a818431fce835be2db58ea9baf9f02cdc0c
Jisoo Song <jeesoo9595@snu.ac.kr>
2025-10-24 15:36:42 +0900

    feat: edge manipulation api
    Add scale_plateau()

--- src/heavyedge/api/__init__.py
+++ src/heavyedge/api/__init__.py
@@ -12,9 +12,10 @@ __all__ = [
     "plateau_type2",
     "plateau_type3",
     "scale_area",
+    "scale_plateau",
 ]
 
-from .edge import scale_area
+from .edge import scale_area, scale_plateau
 from .landmarks import landmarks_type2, landmarks_type3, plateau_type2, plateau_type3
 from .mean import mean_euclidean, mean_wasserstein
 from .profile import fill_after, mean, outlier, preprocess
--- src/heavyedge/api/edge.py
+++ src/heavyedge/api/edge.py
@@ -6,6 +6,7 @@ from .profile import fill_after
 
 __all__ = [
     "scale_area",
+    "scale_plateau",
 ]
 
 
@@ -55,3 +56,43 @@ def _area(x, Ys, Ls):
     Ys = Ys.copy()
     fill_after(Ys, Ls, 0)
     return np.trapezoid(Ys, x, axis=1)
+
+
+def scale_plateau(f, batch_size=None, logger=lambda x: None):
+    """Scale edge profile by plateau height.
+
+    Parameters
+    ----------
+    f : heavyedge.ProfileData
+        Open h5 file of profiles.
+    batch_size : int, optional
+        Batch size to load data.
+        If not passed, all data are loaded at once.
+    logger : callable, optional
+        Logger function which accepts a progress message string.
+
+    Yields
+    ------
+    scaled : (batch_size, M) array
+        Scaled edge profile.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from heavyedge import get_sample_path, ProfileData
+    >>> from heavyedge.api import scale_plateau
+    >>> with ProfileData(get_sample_path("Prep-Type3.h5")) as f:
+    ...     Ys = np.concatenate(list(scale_plateau(f, batch_size=5)), axis=0)
+    """
+    if batch_size is None:
+        Ys, _, _ = f[:]
+        Ys /= Ys[:, [0]]
+        logger("1/1")
+        yield Ys
+    else:
+        N = len(f)
+        for i in range(0, N, batch_size):
+            Ys, _, _ = f[i : i + batch_size]
+            Ys /= Ys[:, [0]]
+            logger(f"{i}/{N}")
+            yield Ys
