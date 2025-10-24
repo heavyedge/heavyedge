20512d0c4e7e5947e1dac54a7431994f03233074
Jisoo Song <jeesoo9595@snu.ac.kr>
2025-10-25 03:38:15 +0900

    doc: modify header plot

--- doc/source/plot-header.py
+++ doc/source/plot-header.py
@@ -1,43 +1,23 @@
 import matplotlib.pyplot as plt
 
 from heavyedge import ProfileData, get_sample_path
-from heavyedge.api import (
-    landmarks_type2,
-    landmarks_type3,
-    mean,
-    plateau_type2,
-    plateau_type3,
-)
+from heavyedge.api import mean_wasserstein
 
 with ProfileData(get_sample_path("Prep-Type2.h5")) as data:
-    x2 = data.x()
-    profiles2 = list(data.profiles())
-mean2 = mean(x2, profiles2, 1000)
-lm2 = landmarks_type2(mean2, 32)
-b0_2, b1_2, psi_2 = plateau_type2(x2[: len(mean2)], mean2, lm2[1], lm2[2])
+    Ys2, _, _ = data[:]
+    mean2, L2 = mean_wasserstein(data, 100)
 
-with ProfileData(get_sample_path("Prep-Type3.h5")) as data:
-    x3 = data.x()
-    profiles3 = list(data.profiles())
-mean3 = mean(x3, profiles3, 1000)
-lm3 = landmarks_type3(mean3, 32)
-b0_3, b1_3, psi_3 = plateau_type3(x3[: len(mean3)], mean3, lm3[2], lm3[3])
+with ProfileData(get_sample_path("Prep-Type2.h5")) as data:
+    Ys3, _, _ = data[:]
+    mean3, L3 = mean_wasserstein(data, 100)
 
 fig, axes = plt.subplots(1, 2, figsize=(10, 4))
 
-for profile in profiles2:
-    axes[0].plot(x2[: len(profile)], profile, alpha=0.1, color="gray")
-axes[0].plot(x2[: len(mean2)], mean2, color="gray")
-axes[0].plot(x2[lm2[:-1]], mean2[lm2[:-1]], "o")
-X2 = x2[x2 < psi_2]
-axes[0].plot(X2, b0_2 + b1_2 * X2)
+axes[0].plot(Ys2.T, alpha=0.1, color="gray")
+axes[0].plot(mean2[:L2])
 
-for profile in profiles3:
-    axes[1].plot(x3[: len(profile)], profile, alpha=0.1, color="gray")
-axes[1].plot(x3[: len(mean3)], mean3, color="gray")
-axes[1].plot(x3[lm3[:-1]], mean3[lm3[:-1]], "o")
-X3 = x3[x3 < psi_3]
-axes[1].plot(X3, b0_3 + b1_3 * X3)
+axes[1].plot(Ys3.T, alpha=0.1, color="gray")
+axes[1].plot(mean3[:L3])
 
 axes[0].axis("off")
 axes[1].axis("off")
