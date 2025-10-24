d43a77c563f05ba009ba5727e9bac187c4be1c46
Jisoo Song <jeesoo9595@snu.ac.kr>
2025-10-24 16:37:43 +0900

    feat: edge manipulation api
    Implement pad()

--- src/heavyedge/api/__init__.py
+++ src/heavyedge/api/__init__.py
@@ -14,9 +14,10 @@ __all__ = [
     "scale_area",
     "scale_plateau",
     "trim",
+    "pad",
 ]
 
-from .edge import scale_area, scale_plateau, trim
+from .edge import pad, scale_area, scale_plateau, trim
 from .landmarks import landmarks_type2, landmarks_type3, plateau_type2, plateau_type3
 from .mean import mean_euclidean, mean_wasserstein
 from .profile import fill_after, mean, outlier, preprocess
--- src/heavyedge/api/edge.py
+++ src/heavyedge/api/edge.py
@@ -8,6 +8,7 @@ __all__ = [
     "scale_area",
     "scale_plateau",
     "trim",
+    "pad",
 ]
 
 
@@ -149,7 +150,67 @@ def trim(f, width=None, batch_size=None, logger=lambda x: None):
 
 def _trim(Ys, Ls, w1, w2):
     N, M = Ys.shape
-    mask1 = np.arange(M)[None, :] >= (Ls - w1)[:, None]
-    mask2 = np.arange(M)[None, :] < (Ls + w2)[:, None]
+    idx_array = np.arange(M)[None, :]
+    mask1 = idx_array >= (Ls - w1)[:, None]
+    mask2 = idx_array < (Ls + w2)[:, None]
     ret = Ys[mask1 & mask2].reshape(N, w1 + w2)
+    return ret
+
+
+def pad(f, width=None, batch_size=None, logger=lambda x: None):
+    """Pad edge profile to a specific width.
+
+    Parameters
+    ----------
+    f : heavyedge.ProfileData
+    width : int
+        Length to pad the profile to.
+        Must be of physical length by `f.x()`.
+        If not passed, set to the length of the shortest profile.
+    batch_size : int, optional
+        Batch size to load data.
+        If not passed, all data are loaded at once.
+    logger : callable, optional
+        Logger function which accepts a progress message string.
+
+    Yields
+    ------
+    padded : (batch_size, width) array
+        Padded edge profile.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from heavyedge import get_sample_path, ProfileData
+    >>> from heavyedge.api import pad
+    >>> with ProfileData(get_sample_path("Prep-Type3.h5")) as f:
+    ...     Ys = np.concatenate(list(pad(f, 20, batch_size=10)), axis=0)
+    """
+    N, M = f.shape()
+    Ls = f._file["len"][:]
+    if width is None:
+        width_num = Ls.max()
+    else:
+        width_num = int(f.resolution() * width)
+    substrate_num = (M - Ls).min()
+
+    if batch_size is None:
+        Ys, Ls, _ = f[:]
+        logger(f"{N}/{N}")
+        yield _pad(Ys, Ls, width_num, substrate_num)
+    else:
+        for i in range(0, N, batch_size):
+            Ys, Ls, _ = f[i : i + batch_size]
+            logger(f"{i}/{N}")
+            yield _pad(Ys, Ls, width_num, substrate_num)
+
+
+def _pad(Ys, Ls, w1, w2):
+    N, M = Ys.shape
+    ret = np.empty((N, w1 + w2), dtype=Ys.dtype)
+
+    ret[:w1] = Ys[:, :1]
+    ret_mask = np.arange(w1 + w2)[None, :] >= (w1 - Ls)[:, None]
+    Ys_mask = np.arange(M)[None, :] < (w2 + Ls)[:, None]
+    ret[ret_mask] = Ys[Ys_mask]
     return ret
